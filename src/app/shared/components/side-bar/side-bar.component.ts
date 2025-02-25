9ab4e67d005af84313cbffe91f20459e56b3ec96
Klaulara <claudia_lara_zamora@hotmail.com>
2025-02-25 08:06:03 -0300

    first commit settings

---
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -0,0 +1,68 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-bar',
+  standalone: false,
+  templateUrl: './side-bar.component.html',
+  styleUrl: './side-bar.component.css'
+})
+export class SideBarComponent {
+
+  mainMenu: {
+    defaultOptions: Array<any>, accessLink: Array<any>
+  } = { defaultOptions: [], accessLink: [] };
+
+  customOptions: Array<any> = [];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.mainMenu.defaultOptions = [
+      {
+        name: 'Home',
+        icon: 'uil uil-estate',
+        router: ['/']
+      },
+      {
+        name: 'Buscar',
+        icon: 'uil uil-search',
+        router: ['/search']
+      },
+      {
+        name: 'Favoritos',
+        icon: 'uil uil-heart',
+        router: ['/favorites']
+      }
+    ];
+    this.mainMenu.accessLink = [
+      {
+        name: 'Crear lista',
+        icon: 'uil-plus-square'
+      },
+      {
+        name: 'Canciones que te gustan',
+        icon: 'uil-heart-medical'
+      }
+    ]
+
+    this.customOptions = [
+      {
+        name: 'Mi lista º1',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista º2',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista º3',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista º4',
+        router: ['/']
+      }
+    ]
+  }
+
+}
